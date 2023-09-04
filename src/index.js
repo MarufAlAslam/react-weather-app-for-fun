14f15316b0a0b4f073ef98eeba8c85f2cf3000fc
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-09-04 11:33:29 +0600

    UI done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './assets/css/globals.css'
 import MainPage from './pages/main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,9 +1,21 @@
 import React from "react";
+// import house from '../assets/img/house.svg'
+import umbrella from "../assets/img/umbrella.gif";
+import logo from "../assets/img/logo.webp";
 
 const MainPage = () => {
   return (
-    <div>
-     Hello There
+    <div className="main-page">
+      <div className="wrapper">
+        <div className="card bg-[#000] p-[40px] rounded-[15px] md:w-2/3 flex justify-between items-center gap-[40px] md:flex-row flex-col">
+          <div className="md:w-1/2 h-full bg-[#4f5764] p-[40px] rounded-[15px] flex justify-center items-center">
+            <img src={umbrella} className="w-2/3" alt="" />
+          </div>
+          <div className="md:w-1/2 h-full p-[40px] rounded-[15px] flex justify-center items-center">
+            <img src={logo} className="w-[100px]" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
